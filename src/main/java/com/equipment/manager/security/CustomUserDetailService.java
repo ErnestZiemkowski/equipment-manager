436c84de5ceebb0e8b5c66ef3b8f9b148316df92
ErnestZiemkowski <ernest.ziemkowski@gmail.com>
2019-08-22 21:05:52 +0200

    Fixes

--- src/main/java/com/equipment/manager/security/CustomUserDetailService.java
+++ src/main/java/com/equipment/manager/security/CustomUserDetailService.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.equipment.manager.model.User;
 import com.equipment.manager.repository.UserRepository;
 
+@Service
 public class CustomUserDetailService implements UserDetailsService {
 
 	@Autowired
